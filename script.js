cef9d8cc38cb72e710f218685cf71d9b77eaa406
Luis Cordova <ageofculturebiz@gmail.com>
2023-01-24 14:19:36 -0500

    add images, add sound effects, updated home screen

--- script.js
+++ script.js
@@ -7,14 +7,27 @@ const ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const startButton = document.querySelector('#start-button');
+
 const starshipImage = new Image();
-starshipImage.src = "";
+starshipImage.src = "/images/spaceship-new-two.png";
 
-const alienImage = new Image();
-alienImage.src = "";
+const alienImageOne = new Image();
+alienImageOne.src = "/images/alien-one.png";
 
-const backgroundImage = new Image();
-backgroundImage.src = "";
+const alienImageTwo = new Image();
+alienImageTwo.src = "/images/alien-two.png";
+
+const alienImageThree = new Image();
+alienImageThree.src = "/images/alien-three.png";
+
+const alienImageFour = new Image();
+alienImageFour.src = "/images/alien-four.png";
+
+const alienImageFive = new Image();
+alienImageFive.src = "/images/alien-five.png";
+
+let alienImageArray = [alienImageOne, alienImageFive, alienImageOne, alienImageOne];
 
 // PLAYER & ALIENS & KEYS & PARTICLES
 
@@ -29,9 +42,9 @@ let keys = {
 
 let starship = {
     x: 100,
-    y: canvas.height/2 - 50,
-    width: 0,
-    height: 0,
+    y: canvas.height/2 - 25,
+    width: 100,
+    height: 100,
     speed: 0,
 
     update: function() {
@@ -40,8 +53,8 @@ let starship = {
     },
 
     draw: function() {
-        ctx.fillStyle = "red";
-        ctx.fillRect(this.x, this.y, 100, 100);
+        //ctx.fillStyle = "red";
+        ctx.drawImage(starshipImage, this.x, this.y, this.width, this.height);
     },
 
     shoot: function() {
@@ -66,7 +79,7 @@ class Laser {
     }
 
     draw() {
-        ctx.fillStyle = "red";
+        ctx.fillStyle = "#70d6cb";
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 
@@ -106,9 +119,10 @@ class Alien {
     constructor (a, b) {
         this.x = 1100 + a;
         this.y = 100 + b;
-        this.width = 30;
-        this.height = 30;
-        this.speed = 5;
+        this.width = 50;
+        this.height = 50;
+        this.speed = .5;
+        this.color = alienImageArray[Math.floor(Math.random() * 4)]
     }
 
     move() {
@@ -117,8 +131,7 @@ class Alien {
     }
 
     draw() {
-        ctx.fillStyle = "green";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.color, this.x, this.y, this.width, this.height);
     }
 
     remove() {
@@ -136,8 +149,9 @@ class Alien {
 function startGame() {
     alienArray = [];
     laserArray = [];
+    particleArray = [];
     starship.x = 100;
-    starship.y = canvas.height/2 - 50;
+    starship.y = canvas.height/2 - 25;
     cancelAnimationFrame(animationId);
     levelOne();
 }
@@ -147,27 +161,49 @@ function startGame() {
 
 function levelOne() {
     animate();
-    createAliensOne();
+    createAliensTwo();
     createParticles();
 }
 
 function animate() {
     animationId = requestAnimationFrame(animate);
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = '#0c0c15';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    starship.update();
 
-    if (keys.left) {
-        starship.speed = -8;
-    } else if (keys.right) {
-        starship.speed = 8;
+    if(starship.y + starship.height > canvas.height) {
+        keys.right = false;
+        if (keys.left) {
+            starship.speed = -12;
+        } else if (keys.right) {
+            starship.speed = 12;
+        } else {
+            starship.speed = 0;
+        }
+    } else if (starship.y < 0){
+        keys.left = false;
+        if (keys.left) {
+            starship.speed = -12;
+        } else if (keys.right) {
+            starship.speed = 12;
+        } else {
+            starship.speed = 0;
+        }
     } else {
-        starship.speed = 0;
+        if (keys.left) {
+            starship.speed = -12;
+        } else if (keys.right) {
+            starship.speed = 12;
+        } else {
+            starship.speed = 0;
+        }
     }
 
     for (let p = 0; p < particleArray.length; p++) {
         if(particleArray[p].x < 0) {
-            particleArray.splice(p, 1)
+            // particleArray.splice(p, 1)
+            particleArray[p].x = 1440 + (Math.random() * canvas.width);
+            particleArray[p].y = Math.random() * canvas.height;
+            particleArray[p].move();
         } else {
             particleArray[p].move();
         }
@@ -191,38 +227,67 @@ function animate() {
         }
     }
 
+    starship.update();
+
     for(let j = 0; j < alienArray.length; j++) {
         if (checkCollisionAlien(alienArray[j]) === true){
             alienArray.splice(j, 1);
+            explosionSound();
         }
     }
 }
 
+let particleColors = ['white','purple','pink','white','white']
+
 function createParticles() {
-    for (let i = 0; i < 60; i++) {
+    for (let i = 0; i < 180; i++) {
         let xValue = Math.random() * canvas.width;
         let yValue = Math.random() * canvas.height;
         let rValue = Math.random() * 3;
-        let cValue = 'white';
+        let cValue = particleColors[Math.round((Math.random() * 4))];
         particleArray.push(new Particle(xValue, yValue, rValue, cValue));
         particleArray[i].draw();
     }
 }
 
 function createAliensOne() {
-    for(let i = 0; i < 8; i++) {
+    for(let i = 0; i < 36; i++) {
 
         if (i === 0) {
-            alienArray.push(new Alien(0, 0));
+            alienArray.push(new Alien(-100, 150));
             alienArray[0].draw();
-        } else if (i < 4) {
-            alienArray.push(new Alien(0, 100 + (alienArray[i-1].y - 100)));
+        } else if (i < 6) {
+            alienArray.push(new Alien(-100, 60 + (alienArray[i-1].y - 100)));
             alienArray[i].draw();
-        } else if (i === 4) {
-            alienArray.push(new Alien(100, 0));
+        } else if (i === 6) {
+            alienArray.push(new Alien(-40, 150));
+            alienArray[i].draw();
+        } else if (i < 12) {
+            alienArray.push(new Alien(-40, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        } else if (i === 12) {
+            alienArray.push(new Alien(20, 150));
+            alienArray[i].draw();
+        } else if (i < 18){
+            alienArray.push(new Alien(20, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        } else if (i === 18) {
+            alienArray.push(new Alien(80, 150));
+            alienArray[i].draw();
+        } else if (i < 24) {
+            alienArray.push(new Alien(80, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        } else if (i === 24) {
+            alienArray.push(new Alien(140, 150));
+            alienArray[i].draw();
+        } else if (i < 30) {
+            alienArray.push(new Alien(140, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        } else if (i === 30){
+            alienArray.push(new Alien(200, 150));
             alienArray[i].draw();
         } else {
-            alienArray.push(new Alien(100, 100 + (alienArray[i-1].y - 100)));
+            alienArray.push(new Alien(200, 60 + (alienArray[i-1].y - 100)));
             alienArray[i].draw();
         }
     
@@ -253,6 +318,67 @@ function checkCollisionLaser(laser) {
     }
 }
 
+function createAliensTwo() {
+    for(let i = 0; i < 55; i++) {
+
+        if (i === 0) {
+            alienArray.push(new Alien(0, 0));
+            alienArray[0].draw();
+        } else if (i < 11) {
+            alienArray.push(new Alien(0, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        } else if (i === 11) {
+            alienArray.push(new Alien(60, 0));
+            alienArray[i].draw();
+        } else if (i < 22) {
+            alienArray.push(new Alien(60, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        } else if (i === 22) {
+            alienArray.push(new Alien(120, 0));
+            alienArray[i].draw();
+        } else if (i < 33){
+            alienArray.push(new Alien(120, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        } else if (i === 33) {
+            alienArray.push(new Alien(180, 0));
+            alienArray[i].draw();
+        } else if (i < 44) {
+            alienArray.push(new Alien(180, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        } else if (i === 44) {
+            alienArray.push(new Alien(240, 0));
+            alienArray[i].draw();
+        } else {
+            alienArray.push(new Alien(240, 60 + (alienArray[i-1].y - 100)));
+            alienArray[i].draw();
+        }
+    
+    }
+}
+
+function playMusic() {
+    let backgroundMusic = new Audio('/audios/Rich in the 80s - DivKid.mp3');
+    backgroundMusic.volume = 0.005;
+    backgroundMusic.play();
+    // some weird favicon icon error here
+}
+
+function laserSound() {
+    let laserShot = new Audio('/audios/laser-one.mp3');
+    laserShot.volume = 0.1;
+    laserShot.play();
+}
+
+function explosionSound() {
+    let boom = new Audio('/audios/explosion-one.wav');
+    boom.volume = 0.1;
+    boom.play();
+}
+
+function hideButton() {
+    startButton.style.visibility = 'hidden'; 
+}
+
 
 
 
@@ -265,6 +391,8 @@ function checkCollisionLaser(laser) {
 window.onload = () => {
     document.getElementById('start-button').onclick = function() {
         startGame();
+        hideButton();
+        playMusic();
     };
 
     document.addEventListener('keydown', e => {
@@ -292,9 +420,10 @@ window.onload = () => {
     document.addEventListener('keydown', e => {
         if (e.keyCode === 70) {
             starship.shoot();
-            //sound
+            laserSound();
         }
     })
 }
 
 console.log(innerWidth);
+console.log(innerHeight);
